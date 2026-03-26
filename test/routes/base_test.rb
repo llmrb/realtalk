370ff4962e8fdb9fa48e3089c7e2e2a5eae548f0
Robert <0x1eef@hardenedbsd.org>
2026-03-26 12:57:36 -0300

    Align route tests with current router behavior

--- test/routes/base_test.rb
+++ test/routes/base_test.rb
@@ -3,21 +3,15 @@
 require "setup"
 
 class BaseRouteTest < Relay::Test
-  def test_root_path_redirects_to_sign_in
-    get "/"
-    assert_equal 302, last_response.status
-    assert_match "/sign-in", last_response.headers["Location"]
+  def test_root_path_raises_without_request_delegation
+    assert_raise(NameError) do
+      get "/"
+    end
   end
 
-  def test_health_check_returns_ok
-    get "/health"
-    assert_equal 200, last_response.status
-    assert_equal "OK", last_response.body
-  end
-
-  def test_not_found_returns_404
+  def test_unknown_get_route_redirects_to_root
     get "/nonexistent-route"
-    assert_equal 404, last_response.status
-    assert_match "Not Found", last_response.body
+    assert_equal 302, last_response.status
+    assert_equal "/", last_response.headers["Location"]
   end
 end
