f37eeed48836e8c7af8ff90533cd8ff0c6c8ff94
Robert <0x1eef@hardenedbsd.org>
2026-03-26 13:03:53 -0300

    Remove obsolete api models route test

--- test/routes/list_models_test.rb
+++ test/routes/list_models_test.rb
@@ -3,11 +3,6 @@
 require "setup"
 
 class ListModelsRouteTest < Relay::Test
-  def test_list_models_route_is_not_defined_under_api_namespace
-    get "/api/models"
-    assert_equal 404, last_response.status
-  end
-
   def test_models_route_redirects_unauthenticated_users_to_sign_in
     get "/models"
     assert_equal 302, last_response.status
